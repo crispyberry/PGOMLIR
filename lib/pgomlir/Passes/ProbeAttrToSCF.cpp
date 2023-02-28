e00fd94854f9efae73ea18e039cee15d8b6fdc9f
crispyberry <ssslvky@gmail.com>
2023-03-01 06:58:18 +0800

    Add tripCount as the custom attribute of scf.for

--- lib/pgomlir/Passes/ProbeAttrToSCF.cpp
+++ lib/pgomlir/Passes/ProbeAttrToSCF.cpp
@@ -2,6 +2,8 @@
 #include "mlir/Dialect/SCF/IR/SCF.h"
 #include "mlir/IR/Builders.h"
 #include "mlir/IR/BuiltinOps.h"
+#include "mlir/Dialect/Arith/IR/Arith.h"
+#include "mlir/Support/MathExtras.h"
 
 #include "pgomlir/Passes/Passes.h"
 
@@ -10,8 +12,8 @@ using namespace pgomlir;
 
 namespace {
 struct MyAttribute {
-  static StringAttr get(MLIRContext *context) {
-    return StringAttr::get(context, "my_string_value");
+  static StringAttr get(MLIRContext *context, llvm::StringRef st) {
+    return StringAttr::get(context, st);
   }
 };
 
@@ -24,12 +26,26 @@ struct ProbeAttrToSCFPass
 void ProbeAttrToSCFPass::runOnOperation() {
   auto ops = getOperation();
   auto &context = getContext();
-  ops->walk([&](scf::ForOp forOp) {
-    // Create the custom attribute.
-    StringAttr attr = MyAttribute::get(&context);
+  ops->walk([&](Operation* op) {
+    auto forOp = dyn_cast<scf::ForOp>(op);
+    if (!forOp)
+      return;
+
+    auto lbCstOp =
+        forOp.getLowerBound().getDefiningOp<arith::ConstantIndexOp>();
+    auto ubCstOp =
+        forOp.getUpperBound().getDefiningOp<arith::ConstantIndexOp>();
+    auto stepCstOp = forOp.getStep().getDefiningOp<arith::ConstantIndexOp>();
+    if (!lbCstOp || !ubCstOp || !stepCstOp || lbCstOp.value() < 0 ||
+        ubCstOp.value() < 0 || stepCstOp.value() < 0)
+      return;
+    int64_t tripCount =
+        mlir::ceilDiv(ubCstOp.value() - lbCstOp.value(), stepCstOp.value());
+    //Create the custom attribute.
+    StringAttr attr = MyAttribute::get(&context, llvm::utostr(tripCount));
 
     // Set the custom attribute on the `scf.for` operation.
-    forOp->setAttr("my_attribute", attr);
+    (*forOp).setAttr("my_attribute", attr);
   });
 }
 
