9b4b3c6462c72522049992a2b9327cd6990da930
crispyberry <ssslvky@gmail.com>
2023-03-01 09:06:28 +0800

    Update Pass adding tripCount as atrribute to scf.for by PatternMatch

--- lib/pgomlir/Passes/ProbeAttrToSCF.cpp
+++ lib/pgomlir/Passes/ProbeAttrToSCF.cpp
@@ -1,9 +1,12 @@
 #include "PassDetails.h"
+
+#include "mlir/Dialect/Arith/IR/Arith.h"
 #include "mlir/Dialect/SCF/IR/SCF.h"
 #include "mlir/IR/Builders.h"
 #include "mlir/IR/BuiltinOps.h"
-#include "mlir/Dialect/Arith/IR/Arith.h"
+#include "mlir/IR/PatternMatch.h"
 #include "mlir/Support/MathExtras.h"
+#include "mlir/Transforms/GreedyPatternRewriteDriver.h"
 
 #include "pgomlir/Passes/Passes.h"
 
@@ -17,38 +20,56 @@ struct MyAttribute {
   }
 };
 
+struct TripCountAttrSCFPattern : public OpRewritePattern<scf::ForOp> {
+  using OpRewritePattern::OpRewritePattern;
+
+  LogicalResult matchAndRewrite(scf::ForOp forOp,
+                                PatternRewriter &rewriter) const override;
+};
+
 struct ProbeAttrToSCFPass
     : public PassWrapper<ProbeAttrToSCFPass, OperationPass<>> {
   void runOnOperation() override;
 };
 } // namespace
 
-void ProbeAttrToSCFPass::runOnOperation() {
-  auto ops = getOperation();
-  auto &context = getContext();
-  ops->walk([&](Operation* op) {
-    auto forOp = dyn_cast<scf::ForOp>(op);
-    if (!forOp)
-      return;
+LogicalResult
+TripCountAttrSCFPattern::matchAndRewrite(scf::ForOp forOp,
+                                         PatternRewriter &rewriter) const {
+  if (forOp->getAttrOfType<StringAttr>("tripCount"))
+    return failure();
+  auto lbCstOp = forOp.getLowerBound().getDefiningOp<arith::ConstantIndexOp>();
+  auto ubCstOp = forOp.getUpperBound().getDefiningOp<arith::ConstantIndexOp>();
+  auto stepCstOp = forOp.getStep().getDefiningOp<arith::ConstantIndexOp>();
+  if (!lbCstOp || !ubCstOp || !stepCstOp || lbCstOp.value() < 0 ||
+      ubCstOp.value() < 0 || stepCstOp.value() < 0) {
+    auto tripCountAttr = StringAttr::get(forOp.getContext(), "unknown");
+    rewriter.updateRootInPlace(
+        forOp, [&]() { forOp->setAttr("tripCount", tripCountAttr); });
+    return failure();
+  }
 
-    auto lbCstOp =
-        forOp.getLowerBound().getDefiningOp<arith::ConstantIndexOp>();
-    auto ubCstOp =
-        forOp.getUpperBound().getDefiningOp<arith::ConstantIndexOp>();
-    auto stepCstOp = forOp.getStep().getDefiningOp<arith::ConstantIndexOp>();
-    if (!lbCstOp || !ubCstOp || !stepCstOp || lbCstOp.value() < 0 ||
-        ubCstOp.value() < 0 || stepCstOp.value() < 0)
-      return;
-    int64_t tripCount =
-        mlir::ceilDiv(ubCstOp.value() - lbCstOp.value(), stepCstOp.value());
-    //Create the custom attribute.
-    StringAttr attr = MyAttribute::get(&context, llvm::utostr(tripCount));
+  int64_t tripCount =
+      ceilDiv(ubCstOp.value() - lbCstOp.value(), stepCstOp.value());
+  auto tripCountAttr =
+      StringAttr::get(forOp.getContext(), llvm::utostr(tripCount));
 
-    // Set the custom attribute on the `scf.for` operation.
-    (*forOp).setAttr("my_attribute", attr);
-  });
+  rewriter.updateRootInPlace(
+      forOp, [&]() { forOp->setAttr("tripCount", tripCountAttr); });
+
+  return success();
 }
 
+void ProbeAttrToSCFPass::runOnOperation() {
+
+  RewritePatternSet patterns(&getContext());
+  patterns.add<TripCountAttrSCFPattern>(&getContext());
+
+  if (failed(
+          applyPatternsAndFoldGreedily(getOperation(), std::move(patterns)))) {
+    signalPassFailure();
+  }
+}
 std::unique_ptr<Pass> mlir::pgomlir::createProbeAttrToSCFPass() {
   return std::make_unique<ProbeAttrToSCFPass>();
 }
