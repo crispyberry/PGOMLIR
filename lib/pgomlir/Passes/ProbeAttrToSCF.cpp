6ff08839a9091370bcc8eef69b46c2379d659555
crispyberry <ssslvky@gmail.com>
2023-03-04 01:36:07 +0800

    Add demo part for getting dynamic step value

--- lib/pgomlir/Passes/ProbeAttrToSCF.cpp
+++ lib/pgomlir/Passes/ProbeAttrToSCF.cpp
@@ -38,63 +38,84 @@ TripCountAttrSCFPattern::matchAndRewrite(scf::ForOp forOp,
                                          PatternRewriter &rewriter) const {
   if (forOp->getAttrOfType<StringAttr>("tripCount"))
     return failure();
+
+  // Firstly assume that bounds for loop are defined well by operations within
+  // arith.
   auto lbCstOp = forOp.getLowerBound().getDefiningOp<arith::ConstantIndexOp>();
   auto ubCstOp = forOp.getUpperBound().getDefiningOp<arith::ConstantIndexOp>();
   auto stepCstOp = forOp.getStep().getDefiningOp<arith::ConstantIndexOp>();
 
   if (!lbCstOp && !ubCstOp && !stepCstOp) {
-    auto tripCountAttr = StringAttr::get(forOp.getContext(), std::string("unknown") );
+    auto tripCountAttr =
+        StringAttr::get(forOp.getContext(), std::string("unknown"));
     rewriter.updateRootInPlace(
         forOp, [&]() { forOp->setAttr("tripCount", tripCountAttr); });
     return failure();
   } else if (lbCstOp && ubCstOp && !stepCstOp) {
-    auto tripCountAttr =
-        StringAttr::get(forOp.getContext(),
-                        llvm::utostr(ubCstOp.value()) + std::string("-")  +
-                            llvm::utostr(lbCstOp.value()) + std::string("/")  + std::string("unknown") );
+    std::string stepName;
+    if (Operation *producer = forOp.getStep().getDefiningOp()) {
+      stepName = "op:" + producer->getName().getStringRef().str();
+    } else {
+      // If there is no defining op, the Value is necessarily a Block
+      // argument.
+      auto blockArg = forOp.getStep().cast<BlockArgument>();
+      stepName = "blockArgIndex:" + llvm::utostr(blockArg.getArgNumber());
+    }
+
+    auto tripCountAttr = StringAttr::get(
+        forOp.getContext(), llvm::utostr(ubCstOp.value()) + std::string("-") +
+                                llvm::utostr(lbCstOp.value()) +
+                                std::string("/") + stepName);
     rewriter.updateRootInPlace(
         forOp, [&]() { forOp->setAttr("tripCount", tripCountAttr); });
     return failure();
   } else if (!lbCstOp && ubCstOp && stepCstOp) {
     auto tripCountAttr = StringAttr::get(
-        forOp.getContext(), llvm::utostr(ubCstOp.value()) + std::string("-")  + std::string("unknown")  +
-                                std::string("/")  + llvm::utostr(stepCstOp.value()));
+        forOp.getContext(), llvm::utostr(ubCstOp.value()) + std::string("-") +
+                                std::string("unknown") + std::string("/") +
+                                llvm::utostr(stepCstOp.value()));
     rewriter.updateRootInPlace(
         forOp, [&]() { forOp->setAttr("tripCount", tripCountAttr); });
     return failure();
   } else if (lbCstOp && !ubCstOp && stepCstOp) {
-    auto tripCountAttr = StringAttr::get(
-        forOp.getContext(), std::string("unknown")  + std::string("-")  + llvm::utostr(lbCstOp.value()) +
-                                std::string("/")  + llvm::utostr(stepCstOp.value()));
+    auto tripCountAttr =
+        StringAttr::get(forOp.getContext(),
+                        std::string("unknown") + std::string("-") +
+                            llvm::utostr(lbCstOp.value()) + std::string("/") +
+                            llvm::utostr(stepCstOp.value()));
     rewriter.updateRootInPlace(
         forOp, [&]() { forOp->setAttr("tripCount", tripCountAttr); });
     return failure();
   } else if (!lbCstOp && !ubCstOp && stepCstOp) {
     auto tripCountAttr = StringAttr::get(
-        forOp.getContext(), std::string("unknown")  + std::string("/")  + llvm::utostr(stepCstOp.value()));
+        forOp.getContext(), std::string("unknown") + std::string("/") +
+                                llvm::utostr(stepCstOp.value()));
     rewriter.updateRootInPlace(
         forOp, [&]() { forOp->setAttr("tripCount", tripCountAttr); });
     return failure();
   } else if (lbCstOp && !ubCstOp && !stepCstOp) {
     auto tripCountAttr = StringAttr::get(
-        forOp.getContext(),
-        std::string("unknown")  + std::string("-")  + llvm::utostr(lbCstOp.value()) + std::string("/")  + std::string("unknown") );
+        forOp.getContext(), std::string("unknown") + std::string("-") +
+                                llvm::utostr(lbCstOp.value()) +
+                                std::string("/") + std::string("unknown"));
     rewriter.updateRootInPlace(
         forOp, [&]() { forOp->setAttr("tripCount", tripCountAttr); });
     return failure();
   } else if (!lbCstOp && ubCstOp && !stepCstOp) {
-    auto tripCountAttr = StringAttr::get(forOp.getContext(),
-                                         llvm::utostr(ubCstOp.value()) + std::string("-")  +
-                                             std::string("unknown")  + std::string("/")  + std::string("unknown") );
+    auto tripCountAttr = StringAttr::get(
+        forOp.getContext(), llvm::utostr(ubCstOp.value()) + std::string("-") +
+                                std::string("unknown") + std::string("/") +
+                                std::string("unknown"));
     rewriter.updateRootInPlace(
         forOp, [&]() { forOp->setAttr("tripCount", tripCountAttr); });
     return failure();
   } else if (lbCstOp.value() < 0 || ubCstOp.value() < 0 ||
              stepCstOp.value() < 0) {
-    auto tripCountAttr = StringAttr::get(
-        forOp.getContext(), llvm::utostr(ubCstOp.value()) + std::string("-")  +
-                                llvm::utostr(lbCstOp.value()) + std::string("/")  +
-                                llvm::utostr(stepCstOp.value()));
+    auto tripCountAttr =
+        StringAttr::get(forOp.getContext(),
+                        llvm::utostr(ubCstOp.value()) + std::string("-") +
+                            llvm::utostr(lbCstOp.value()) + std::string("/") +
+                            llvm::utostr(stepCstOp.value()));
     rewriter.updateRootInPlace(
         forOp, [&]() { forOp->setAttr("tripCount", tripCountAttr); });
     return failure();
